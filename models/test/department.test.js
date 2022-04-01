a2316bb8026156cc1d2d00e3fb710c7bf622ea4c
Marcin Nowak <maarcin.nowaak@gmail.com>
2022-04-01 11:28:58 +0200

    Add 30.2 - Mongoose models testing - next test

--- models/test/department.test.js
+++ models/test/department.test.js
@@ -8,12 +8,26 @@ describe('Department', () => {
     mongoose.models = {};
   });
 
-  it('should throw an error if no "name" arg', () => {
+  it('TEST 001 - should throw an error if no "name" arg', () => {
     const dep = new Department({}); // create new Department, but don't set `name` attr value
   
     dep.validate(err => {
       expect(err.errors.name).to.exist;
     });
+  
+  });
+
+  it('TEST 002 - should throw an error if "name" is not a string', () => {
+
+    const cases = [{}, []];
+    for(let name of cases) {
+      const dep = new Department({ name });
+  
+      dep.validate(err => {
+        expect(err.errors.name).to.exist;
+      });
+  
+    }
   
   });
 
