b9574f546c6f886452cd93b3365c465ffd550543
Marcin Nowak <maarcin.nowaak@gmail.com>
2022-04-01 11:32:39 +0200

    Add 30.2 - Mongoose models testing - more requirments - more tests

--- models/test/department.test.js
+++ models/test/department.test.js
@@ -31,4 +31,18 @@ describe('Department', () => {
   
   });
 
+  it('TEST 003 - should throw an error if "name" is too short or too long', () => {
+
+    const cases = ['Abc', 'abcd', 'Lorem Ipsum, Lorem Ip']; // we test various cases, some of them are too short, some of them are too long
+    for(let name of cases) {
+      const dep = new Department({ name });
+  
+      dep.validate(err => {
+        expect(err.errors.name).to.exist;
+      });
+  
+    }
+  
+  })
+
 });
